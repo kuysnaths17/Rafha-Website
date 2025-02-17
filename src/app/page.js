10d7dedc6540db0170cecd42c52d83621bba5ca3
kuysnaths <kuysnaths17@gmail.com>
2025-02-17 11:39:56 +0800

    initial

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="flex-1">
+      <div className="justify-center text-center text-4xl self-center">
+      rafhalicious
+      </div>
+      
+    </div>
+  )
+}
