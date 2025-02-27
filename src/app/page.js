3022d5cb7fd399a5f732de4939b004c3767b3825
kuysnaths17 <kuysnaths17@gmail.com>
2025-02-27 09:38:28 +0800

    Latest commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <div className="flex-1">
       <div className="justify-center text-center text-4xl self-center">
-      rafhalicious
+      LATEST
       </div>
       
     </div>
